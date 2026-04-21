d7a880024aa2da2213d4b6d268f59bbf7bd4ede6
Samasth Nayak <samasthnayak1@gmail.com>
2026-04-21 04:03:49 +0000

    fixed api routes

--- src/types/endpoints.ts
+++ src/types/endpoints.ts
@@ -1,22 +1,22 @@
 export enum ApiEndpoints {
-    LOGIN = '/api/v1/auth/login',
-    VALIDATE_SESSION = '/api/v1/auth/validate-token',
-    SIGNUP = '/api/v1/auth/signup',
-    REFRESH_TOKEN = '/api/v1/auth/refresh',
-    DASHBOARD_ITEMS = '/api/v1/items',
-    IDEMPOTENT_KEY = '/api/v1/bids/idempotent',
-    CREATE_BID = '/api/v1/bids/create',
-    BID_HISTORY = '/api/v1/bids/all/',
-    GET_ITEMS= '/api/v1/items',
-    GET_CATEGORIES = '/api/v1/categories',
-    CREATE_ITEM = '/api/v1/items',
-    GET_PRESIGNED_URL = '/api/v1/items/file/presigned-url',
+    LOGIN = '/api/auth/v1/login',
+    VALIDATE_SESSION = '/api/auth/v1/validate-token',
+    SIGNUP = '/api/auth/v1/signup',
+    REFRESH_TOKEN = '/api/auth/v1/refresh',
+    DASHBOARD_ITEMS = '/api/auth/v1/items',
+    IDEMPOTENT_KEY = '/api/auth/v1/bids/idempotent',
+    CREATE_BID = '/api/auth/v1/bids/create',
+    BID_HISTORY = '/api/auth/v1/bids/all/',
+    GET_ITEMS= '/api/auth/v1/items',
+    GET_CATEGORIES = '/api/auth/v1/categories',
+    CREATE_ITEM = '/api/auth/v1/items',
+    GET_PRESIGNED_URL = '/api/auth/v1/items/file/presigned-url',
 
-    ADD_PICTURES = '/api/v1/items/pictures',
+    ADD_PICTURES = '/api/auth/v1/items/pictures',
 
-    GET_USERS_BIDS = '/api/v1/bids/all/user',
+    GET_USERS_BIDS = '/api/auth/v1/bids/all/user',
 
-    SEARCH_ITEMS = '/api/v1/items/search',
+    SEARCH_ITEMS = '/api/auth/v1/items/search'
 
 
 
